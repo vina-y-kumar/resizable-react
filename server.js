4a5a485c8ba5bb186f7afec10e9605f41f54afdf
Vinay Kumar <92167129+vina-y-kumar@users.noreply.github.com>
2024-03-03 10:37:05 +0530

    update

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const path=require('path');
+const path=require ('path');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
